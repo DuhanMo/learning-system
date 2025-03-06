56858386c097dff076b11db40be5cee7fccb6724
Duhan Mo <duhandv@gmail.com>
2025-03-06 22:35:21 +0900

    [리팩터링 2판] 1장 test: 초기 코드에 대한 테스트 작성

---
+++ programming-book/refactoring-2nd/project/src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,18 @@
+import duhan.io.invoices
+import duhan.io.plays
+import duhan.io.statement
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+    @Test
+    fun statementTest() {
+        val expected = "청구 내역 (고객명: BigCo)\n" +
+                " hamlet: ${'$'}650.00 (55석)\n" +
+                " As You Like It: ${'$'}580.00 (35석)\n" +
+                " Othello: ${'$'}500.00 (40석)\n" +
+                "총액: ${'$'}1,730.00\n" +
+                "적립 포인트: 47점\n"
+        assertEquals(expected, statement(invoices[0], plays))
+    }
+}
