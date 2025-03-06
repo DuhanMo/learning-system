23b85ad1c2abdfdbcfca2fe19fd6342cbe7f0c66
Duhan Mo <duhandv@gmail.com>
2025-03-06 22:23:17 +0900

    [리팩터링 2판] 1장 feat: 초기 코드 작성

---
+++ programming-book/refactoring-2nd/project/src/main/kotlin/Main.kt
@@ -0,0 +1,99 @@
+package duhan.io
+
+import java.text.NumberFormat
+import java.util.Locale
+import kotlin.math.max
+
+val plays = mapOf(
+    "hamlet" to Play(name = "hamlet", type = "tragedy"),
+    "as-like" to Play(name = "As You Like It", type = "comedy"),
+    "othello" to Play(name = "Othello", type = "tragedy"),
+)
+
+val invoices = listOf(
+    Invoice(
+        customer = "BigCo",
+        performances = listOf(
+            Performance(
+                playID = "hamlet",
+                audience = 55,
+            ),
+            Performance(
+                playID = "as-like",
+                audience = 35
+            ),
+            Performance(
+                playID = "othello",
+                audience = 40
+            )
+        )
+    )
+)
+
+fun statement(invoice: Invoice, plays: Map<String, Play>): String {
+    var totalAmount = 0
+    var volumeCredits = 0
+    var result = "청구 내역 (고객명: ${invoice.customer})\n"
+
+    fun format(amount: Number): String {
+        val formatter = NumberFormat.getCurrencyInstance(Locale.US)
+        formatter.minimumFractionDigits = 2
+        return formatter.format(amount.toDouble())
+    }
+
+    for (perf in invoice.performances) {
+        val play = plays[perf.playID]!!
+        var thisAmount = 0
+
+        when (play.type) {
+            "tragedy" -> { // 비극
+                thisAmount = 40_000
+                if (perf.audience > 30) {
+                    thisAmount += 1_000 * (perf.audience - 30)
+                }
+            }
+
+            "comedy" -> { // 희극
+                thisAmount = 30_000
+                if (perf.audience > 20) {
+                    thisAmount += 10_000 + 500 * (perf.audience - 20)
+                }
+                thisAmount += 300 * perf.audience
+            }
+
+            else -> throw IllegalArgumentException("알 수 없는 장르: ${play.type}")
+        }
+        // 포인트를 적립한다.
+        volumeCredits += max(perf.audience - 30, 0)
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if ("comedy" == play.type) volumeCredits += (perf.audience / 5)
+        // 청구 내역을 출력한다.
+        result += " ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n"
+        totalAmount += thisAmount
+    }
+    result += "총액: ${format(totalAmount / 100)}\n"
+    result += "적립 포인트: ${volumeCredits}점\n"
+    return result
+}
+
+
+fun main() {
+    val result = statement(invoices[0], plays)
+    println(result)
+}
+
+
+data class Play(
+    val name: String,
+    val type: String,
+)
+
+data class Invoice(
+    val customer: String,
+    val performances: List<Performance>
+)
+
+data class Performance(
+    val playID: String,
+    val audience: Int,
+)
