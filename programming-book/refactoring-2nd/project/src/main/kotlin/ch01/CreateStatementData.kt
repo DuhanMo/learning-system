3da97dc7f2e2fcf145fc13c4f575a641135c0113
Duhan Mo <duhandv@gmail.com>
2025-03-12 21:29:59 +0900

    [리팩터링 2판] 1장 refactor: 공연계산기 클래스를 통한 금액 계산

--- programming-book/refactoring-2nd/project/src/main/kotlin/ch01/CreateStatementData.kt
+++ programming-book/refactoring-2nd/project/src/main/kotlin/ch01/CreateStatementData.kt
@@ -14,42 +14,18 @@ fun createStatementData(invoice: Invoice): StatementData {
 
 fun enrichPerformance(aPerformance: Performance): EnrichedPerformance {
     val play = playFor(aPerformance)
+    val calculator = PerformanceCalculator(aPerformance, play)
     return EnrichedPerformance(
         playID = aPerformance.playID,
         audience = aPerformance.audience,
-        play = play,
-        amount = amountFor(aPerformance, play),
+        play = calculator.play,
+        amount = calculator.amount(),
         volumeCredits = volumeCreditsFor(aPerformance, play),
     )
 }
 
 fun playFor(aPerformance: Performance): Play {
     return plays[aPerformance.playID]!!
-}
-
-fun amountFor(aPerformance: Performance, play: Play): Int {
-    var result: Int
-
-    when (play.type) {
-        "tragedy" -> { // 비극
-            result = 40_000
-            if (aPerformance.audience > 30) {
-                result += 1_000 * (aPerformance.audience - 30)
-            }
-        }
-
-        "comedy" -> { // 희극
-            result = 30_000
-            if (aPerformance.audience > 20) {
-                result += 10_000 + 500 * (aPerformance.audience - 20)
-            }
-            result += 300 * aPerformance.audience
-        }
-
-        else -> throw IllegalArgumentException("알 수 없는 장르: ${play.type}")
-    }
-
-    return result
 }
 
 fun volumeCreditsFor(aPerformance: Performance, play: Play): Int {
@@ -65,3 +41,33 @@ fun volumeCreditsFor(aPerformance: Performance, play: Play): Int {
 fun totalAmount(performances: List<EnrichedPerformance>): Int = performances.sumOf { it.amount }
 
 fun totalVolumeCredits(performances: List<EnrichedPerformance>): Int = performances.sumOf { it.volumeCredits }
+
+class PerformanceCalculator(
+    val performance: Performance,
+    val play : Play,
+) {
+    fun amount(): Int {
+        var result: Int
+
+        when (play.type) {
+            "tragedy" -> { // 비극
+                result = 40_000
+                if (performance.audience > 30) {
+                    result += 1_000 * (performance.audience - 30)
+                }
+            }
+
+            "comedy" -> { // 희극
+                result = 30_000
+                if (performance.audience > 20) {
+                    result += 10_000 + 500 * (performance.audience - 20)
+                }
+                result += 300 * performance.audience
+            }
+
+            else -> throw IllegalArgumentException("알 수 없는 장르: ${play.type}")
+        }
+
+        return result
+    }
+}
