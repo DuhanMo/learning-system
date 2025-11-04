08ca47bb28fd7834459c61d5f822cb246d92362f
Duhan Mo <duhandv@gmail.com>
2025-11-04 23:51:55 +0900

    [Spring Batch] 프로젝트 셋업

---
+++ leture/inflearn/batch/kill-batch-system/src/main/kotlin/com/system/batch/killbatchsystem/KillBatchSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.system.batch.killbatchsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KillBatchSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<KillBatchSystemApplication>(*args)
+}
