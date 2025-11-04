05df36f1c420f09c50659cf50bf2a1c1e29b2914
Duhan Mo <duhandv@gmail.com>
2025-11-04 23:52:18 +0900

    [Spring Batch] 프로젝트 셋업2(배치권장셋업) - 배치 작업의 성공/실패 상태를 exit code로 외부 시스템에 전달

--- leture/inflearn/batch/kill-batch-system/src/main/kotlin/com/system/batch/killbatchsystem/KillBatchSystemApplication.kt
+++ leture/inflearn/batch/kill-batch-system/src/main/kotlin/com/system/batch/killbatchsystem/KillBatchSystemApplication.kt
@@ -1,11 +1,15 @@
 package com.system.batch.killbatchsystem
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import kotlin.system.exitProcess
 
 @SpringBootApplication
 class KillBatchSystemApplication
 
 fun main(args: Array<String>) {
-	runApplication<KillBatchSystemApplication>(*args)
+	val context = runApplication<KillBatchSystemApplication>(*args)
+	val exitCode = SpringApplication.exit(context)
+    exitProcess(exitCode)
 }
